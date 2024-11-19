8cb96b232b1159c011f5b47676f945891305d9dc
Maxim Sur <makarka16@gmail.com>
2024-11-19 18:43:19 +0200

    Add: redux toolkit

--- src/components/AllRecipes/Pagination.tsx
+++ src/components/AllRecipes/Pagination.tsx
@@ -4,7 +4,7 @@ const Pagination = () => {
   return (
     <div className='desc_text pagination_container'>
       <button className='pagination_item'>&lt;</button>
-      <div className="flex gap-4">
+      <div className="hidden gap-4 sm:flex">
         <button className='pagination_item pagination_item_active'>1</button>
         <button className='pagination_item'>2</button>
         <button className='pagination_item'>3</button>
